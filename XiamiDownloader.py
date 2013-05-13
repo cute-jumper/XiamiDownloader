db3aaeec9cd015384ce0b0cba89977962c6581ec
qjp <qjp-ch-mail@163.com>
2013-05-13 22:34:07 +0800

    Finish version 1.0 and add README.org.

--- XiamiDownloader.py
+++ XiamiDownloader.py
@@ -3,9 +3,10 @@
 # Author: qjp
 # Date: <2013-05-13 Mon>
 
-import sys, math, urllib, urllib2
+from __future__ import division
+import sys, math, urllib, urllib2, time
 from xml.dom.minidom import parse
-from itertools import izip_longest, takewhile, ifilter
+from itertools import izip_longest, takewhile
 from operator import add
 from bs4 import BeautifulSoup as BS
 
@@ -13,10 +14,17 @@ search_url_template = 'http://www.xiami.com/search?key={keyword}'
 search_result_classes = [u'song_name', u'song_artist', u'song_album']
 search_result_classes_zh = [u'歌曲', u'歌手', u'专辑']
 search_result_col_width = 20
+
 xml_url_template = 'http://www.xiami.com/song/playlist/id/{song_id}/object_name/default/object_id/0'
 xml_tagname_list = ['title', 'artist', 'location', 'lyric', 'pic']
+
 request_headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:20.0) Gecko/20100101 Firefox/20.0'}
 
+def xiami_urlopen(url):
+    '''Xiami will not allow using bare urlopen.
+    We have to add headers to the request.'''
+    req = urllib2.Request(url, headers=request_headers)
+    return urllib2.urlopen(req)
 
 def decode_location(loc):
     '''Decode the location. Just `transpose' the matrix'''
@@ -35,11 +43,8 @@ def decode_location(loc):
     real_url = urllib.unquote(clear_text).replace('^', '0').replace('+', ' ')
     return real_url
 
-def xiami_urlopen(url):
-    req = urllib2.Request(url, headers=request_headers)
-    return urllib2.urlopen(req)
-
-def parse_song_info(song_id):    
+def parse_song_info(song_id):
+    '''Parse song information according to the song_id'''
     def get_text(nodelist):
         rc = []
         for node in nodelist:
@@ -53,21 +58,30 @@ def parse_song_info(song_id):
     node_dict['location'] = decode_location(node_dict['location'])
     return node_dict
 
+def search_xiami(keyword):
+    '''Search http://www.xiami.com and return search results'''
+    search_url = search_url_template.format(keyword=keyword)
+    soup = BS(xiami_urlopen(search_url).read())
+    box = soup.find('div', {'class': 'search_result_box'})
+    # Get all song ids
+    song_ids = map(lambda x: (int)(x.children.next()['value']),
+                   box.find_all('td', {'class': 'chkbox'}))
+    # Get all song texts
+    elems = [map(lambda elem: elem.text, box.find_all('td', {'class': classname}))
+             for classname in search_result_classes]
+    return song_ids, zip(*elems)
+
 def output_song_info(song_info):
     for i in xml_tagname_list:
         print '\033[1;36m[' + i + ']:\033[0m ' + song_info[i]
     print
-    
+
 def output_search_results(results):
-    max_num_len = len(str(len(results)))
-    if max_num_len < 2:
-        max_num_len = 2
-    horizon_line = '|-' + '-' * max_num_len + '-+-' + '-+-'.join(['-' * search_result_col_width] * len(search_result_classes)) + '-|'
-    end_line = '--' + '-' * max_num_len + '---' + '---'.join(['-' * search_result_col_width] * len(search_result_classes)) + '--'
-    blank_cell = ' ' * search_result_col_width
+    # Helper functions for output only
     import unicodedata
     def display_len(s):
-        return sum(map(lambda x: 2 if unicodedata.east_asian_width(x) == 'W' or unicodedata.east_asian_width(x) == 'F'
+        return sum(map(lambda x: 2 if unicodedata.east_asian_width(x) == 'W' or
+                       unicodedata.east_asian_width(x) == 'F'
                        else 1, s))
     def scanl(f, acc, l):
         for x in l:
@@ -76,10 +90,12 @@ def output_search_results(results):
     def get_display_len_array(text):
         return [0] + list(scanl(add, 0, map(display_len, text)))
     def get_row_text_generator(text, display_len_array):
+        '''Return the generator for a certain column'''
         def get_row_text():
             start = 0
             while start < len(display_len_array) - 1:
-                length_array = list(takewhile(lambda x: x - display_len_array[start] <= search_result_col_width, display_len_array[start:]))
+                length_array = list(takewhile(lambda x: x - display_len_array[start] <=
+                                              search_result_col_width, display_len_array[start:]))
                 end = start + len(length_array) - 1
                 ret = text[start:end]
                 start = end
@@ -87,10 +103,21 @@ def output_search_results(results):
             while True:
                 yield blank_cell
         return get_row_text()
-    print end_line
-    print '| No | ' + ' | '.join([i + (search_result_col_width - display_len(i)) * ' '
-                          for i in search_result_classes_zh]) + ' |'
-    
+    def build_line(beginning, conjuction, ending, lst):
+        return beginning + conjuction.join(lst) + ending
+    # Helper functions end
+    max_num_len = len(str(len(results)))
+    if max_num_len < 2:
+        max_num_len = 2
+    horizon_line = build_line('|-', '-+-', '-|', ['-' * max_num_len] +
+                              ['-' * search_result_col_width] * len(search_result_classes))
+    celling_line = floor_line = build_line('--', '---', '--', ['-' * max_num_len] +
+                                           ['-' * search_result_col_width] * len(search_result_classes))
+    blank_cell = ' ' * search_result_col_width
+    print celling_line
+    print build_line('| ', ' | ', ' |', ['No%s' %(' ' * (max_num_len - 2))] +
+                     [i + (search_result_col_width - display_len(i)) * ' '
+                      for i in search_result_classes_zh])
     for row_num, res in enumerate(results, 1):
         print horizon_line
         row_text_generators = map(lambda x: get_row_text_generator(x, get_display_len_array(x)), res)
@@ -99,39 +126,96 @@ def output_search_results(results):
             col_text_array = map(lambda x: x.next(), row_text_generators)
             if len(filter(lambda x: x != blank_cell, col_text_array)) > 0:
                 if is_first:
-                    print '| %d%s | ' %(row_num, ' ' * (max_num_len - len(str(row_num)))) + ' | '.join(col_text_array) + ' |'
+                    print build_line('| ', ' | ', ' |', ['%d%s' %(row_num, ' ' * (max_num_len - len(str(row_num))))]
+                                     + col_text_array)
                     is_first = False
                 else:
-                    print '| %s | ' %(' ' * max_num_len) + ' | '.join(col_text_array) + ' |'
+                    print build_line('| ', ' | ', ' |', ['%s' %(' ' * max_num_len)] + col_text_array)
             else:
                 break
-    print end_line
-            
-def search_xiami(keyword):
-    search_url = search_url_template.format(keyword=keyword)
-    soup = BS(xiami_urlopen(search_url).read())
-    box = soup.find('div', {'class': 'search_result_box'})
-    chkboxes = box.find_all('td', {'class': 'chkbox'})
-    elems = [map(lambda elem: elem.text, box.find_all('td', {'class': classname})) for classname in search_result_classes]
-    return map(lambda x: (int)(x.children.next()['value']), chkboxes), zip(*elems)
+    print floor_line
     
+def timeit(method):
+    def timed(*args, **kw):
+        ts = time.time()
+        result = method(*args, **kw)
+        te = time.time()
+        return result, te - ts
+    return timed
+
+def humanize_bytes(bytes, precision=1):
+    """Copied from
+    http://code.activestate.com/recipes/577081-humanized-representation-of-a-number-of-bytes/"""
+    abbrevs = (
+        (1<<50L, 'PB'),
+        (1<<40L, 'TB'),
+        (1<<30L, 'GB'),
+        (1<<20L, 'MB'),
+        (1<<10L, 'kB'),
+        (1, 'B')
+    )
+    if bytes == 1:
+        return '1 byte'
+    for factor, suffix in abbrevs:
+        if bytes >= factor:
+            break
+    return '%.*f %s' % (precision, bytes / factor, suffix)
+
+# My exceptions
+class InputException(Exception):
+    pass
+class NumberRangeException(Exception):
+    pass
+
+def get_user_input():
+    while True:
+        try:
+            user_input = raw_input('Please input a song number("q" to quit): ')
+            if user_input.lower() == 'q':
+                raise InputException()
+            else:
+                sel_num = int(user_input)
+                if 1 <= sel_num <= len(search_results):
+                    break
+                raise NumberRangeException()
+        except (KeyboardInterrupt, InputException):
+            sys.exit(1)
+        except NumberRangeException:
+            print "Error. Please input a number between {low} and {high}!".format(low=1, high=len(search_results))
+        except:
+            print "Error. Please input a number again!"
+    return sel_num
+
+def download_music(sel_result, sel_id):
+    def download_progress(count, block_size, total_size):
+        total_len = 41
+        percent = int(count * block_size * 100 / total_size)
+        finished = '-' * int(percent / 100.0 * (total_len - 1)) + '>'
+        unfinished = ' ' * (total_len - len(finished))
+        sys.stdout.write(chr(27) + '[2K') # clear to end of line, interesting
+        sys.stdout.write("\r%3d%% [%s%s] [%s]" %(percent, finished, unfinished,
+                                                 humanize_bytes(count * block_size)))
+        sys.stdout.flush()
+    saved_file_name = "%s-%s.mp3" %(sel_result[0], sel_result[1])
+    (retrieve_ret, used_time) = timeit(urllib.urlretrieve)(parse_song_info(sel_id)['location'],
+                                                          saved_file_name,
+                                                          download_progress)
+    download_size = long(retrieve_ret[1]['content-length'])
+    print '\nFetched %s in %.1fs (%s/s)' %(humanize_bytes(download_size),
+                                           used_time,
+                                           humanize_bytes(1.0 * download_size / used_time))
+    print "Saved to file '%s'" %saved_file_name
+
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         print '\033[0;32m[Usage]:\033[0m python ' + __file__ + " keyword"
         sys.exit(1)
-    print len(sys.argv)
     (result_ids, search_results) = search_xiami(sys.argv[1])
+    if len(search_results) == 0:
+        print 'No results found!'
+        sys.exit(0)
     output_search_results(search_results)
-    while True:
-        try:
-            input_num = (int)(raw_input('Please input a song number: '))
-            if input_num < 1 or input_num > len(search_results):
-                raise Exception
-            break
-        except:
-            print "Error. Please input a number again!"
-    sel = search_results[input_num - 1]
-    print 'Download begins...Please wait'
-    urllib.urlretrieve(parse_song_info(result_ids[input_num - 1])['location'], "%s_%s.mp3" %(sel[0], sel[1]))
-    print 'Finished!'
-    
+    sel_num = get_user_input()
+    sel_result = search_results[sel_num - 1]
+    sel_id = result_ids[sel_num - 1]
+    download_music(sel_result, sel_id)
